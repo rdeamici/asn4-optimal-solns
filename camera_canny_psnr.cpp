13130b6d9a5c481956fcff51e1167c634a2f4d1c
Rick DeAmicis <rdeamici@uci.edu>
2022-11-17 21:44:04 -0800

    hard code rows and cols

--- camera_canny_psnr.cpp
+++ camera_canny_psnr.cpp
@@ -95,17 +95,15 @@ int main(int argc, char **argv)
          printf("Failed to open media\n");
         return 0;
       }
-   	cap.set(CAP_PROP_FRAME_WIDTH, WIDTH); // Set input resolution when the video is captured from /dev/video*, i.e. the webcam.
-      cap.set(CAP_PROP_FRAME_HEIGHT, HEIGHT);
    }
    else if(!cap.open(infile)) {
       printf("Failed to open media\n");
       return 0;
    }
-   rows = cap.get(CAP_PROP_FRAME_HEIGHT);
-   cols = cap.get(CAP_PROP_FRAME_WIDTH);
-   printf("Media Input: %.0f, %.0f\n", cols, rows);
-
+   cap.set(CAP_PROP_FRAME_WIDTH, WIDTH); // Set input resolution when the video is captured from /dev/video*, i.e. the webcam.
+   cap.set(CAP_PROP_FRAME_HEIGHT, HEIGHT);
+   printf("Media Input: %.0f, %.0f\n",cap.get(CAP_PROP_FRAME_WIDTH), cap.get(CAP_PROP_FRAME_WIDTH));
+   printf("rows = %d, cols = %d",rows, cols);
     // For low-end CPUs, may wait a while until camera stabilizes
    printf("Sleep 3 seconds for camera stabilization...\n");
    usleep(3*1e6);
