29152903453cd6a063d88d6348bd6730b37dd5cc
Rick DeAmicis <rdeamici@uci.edu>
2022-11-20 19:57:24 -0800

    fixed tiger face

--- tiger_face_experiment.sh
+++ tiger_face_experiment.sh
@@ -3,7 +3,7 @@ rm SMALL/*.pgm
 rm MEDIUM/*.pgm
 rm LARGE/*.pgm
 
-./psnr 1 0.2 0.6 200 200 -f tiger_face.jpg >> tiger_face_experiment.log
+./psnr 1 0.2 0.6 200 200 -f tiger_face.jpg > tiger_face_experiment.log
 ./psnr 1 0.2 0.6 640 480 -f tiger_face.jpg >> tiger_face_experiment.log
 ./psnr 1 0.2 0.6 1500 1350 -f tiger_face.jpg >> tiger_face_experiment.log
 
