bef64c787c434ecbed8740d4c2868ce3e2f68b67
rdeamici <rtdeamicis@gmail.com>
2022-11-20 17:43:09 -0800

    fixed experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -45,9 +45,9 @@ rm LARGE/*.pgm
 ./psnr 1   0.9 0.6 200 200   >> realtime_experiment.log
 ./psnr 1   0.9 0.6 640 480   >> realtime_experiment.log
 ./psnr 1   0.9 0.6 1500 1350 >> realtime_experiment.log
-./psnr 1   0.9 0.6 200 200  -f ground_crew.h264 >> realtime_experiment.log
-./psnr 1   0.9 0.6 640 480 -f ground_crew.h264 >> realtime_experiment.log
-./psnr 1   0.9 0.6 1500 1350 -f ground_crew.h264 >> realtime_experiment.log
+./psnr 1   0.9 0.6 200 200  -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 1   0.9 0.6 640 480 -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 1   0.9 0.6 1500 1350 -f ground_crew.h264 >> ground_crew_experiment.log
 rm SMALL/*.pgm
 rm MEDIUM/*.pgm
 rm LARGE/*.pgm
@@ -64,9 +64,9 @@ rm LARGE/*.pgm
 ./psnr 1   0.9 0.4 200 200 >> realtime_experiment.log
 ./psnr 1   0.9 0.4 640 480 >> realtime_experiment.log
 ./psnr 1   0.9 0.4 1500 1350 >> realtime_experiment.log
-./psnr 1   0.9 0.4 200 200 -f ground_crew.h264 >> realtime_experiment.log
-./psnr 1   0.9 0.4 640 480 -f ground_crew.h264 >> realtime_experiment.log
-./psnr 1   0.9 0.4 1500 1350 -f ground_crew.h264 >> realtime_experiment.log
+./psnr 1   0.9 0.4 200 200 -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 1   0.9 0.4 640 480 -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 1   0.9 0.4 1500 1350 -f ground_crew.h264 >> ground_crew_experiment.log
 rm SMALL/*.pgm
 rm MEDIUM/*.pgm
 rm LARGE/*.pgm
@@ -83,9 +83,9 @@ rm LARGE/*.pgm
 ./psnr 0.5 0.2 0.6 200 200 >> realtime_experiment.log
 ./psnr 0.5 0.2 0.6 640 480 >> realtime_experiment.log
 ./psnr 0.5 0.2 0.6 1500 1350 >> realtime_experiment.log
-./psnr 0.5 0.2 0.6 200 200  -f ground_crew.h264 >> realtime_experiment.log
-./psnr 0.5 0.2 0.6 640 480  -f ground_crew.h264 >> realtime_experiment.log
-./psnr 0.5 0.2 0.6 1500 1350  -f ground_crew.h264 >> realtime_experiment.log
+./psnr 0.5 0.2 0.6 200 200  -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 0.5 0.2 0.6 640 480  -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 0.5 0.2 0.6 1500 1350  -f ground_crew.h264 >> ground_crew_experiment.log
 rm SMALL/*.pgm
 rm MEDIUM/*.pgm
 rm LARGE/*.pgm
@@ -102,9 +102,9 @@ rm LARGE/*.pgm
 ./psnr 0.5 0.2 0.4 200 200 >> realtime_experiment.log
 ./psnr 0.5 0.2 0.4 640 480 >> realtime_experiment.log
 ./psnr 0.5 0.2 0.4 1500 1350 >> realtime_experiment.log
-./psnr 0.5 0.2 0.4 200 200  -f ground_crew.h264 >> realtime_experiment.log
-./psnr 0.5 0.2 0.4 640 480  -f ground_crew.h264 >> realtime_experiment.log
-./psnr 0.5 0.2 0.4 1500 1350  -f ground_crew.h264 >> realtime_experiment.log
+./psnr 0.5 0.2 0.4 200 200  -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 0.5 0.2 0.4 640 480  -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 0.5 0.2 0.4 1500 1350  -f ground_crew.h264 >> ground_crew_experiment.log
 rm SMALL/*.pgm
 rm MEDIUM/*.pgm
 rm LARGE/*.pgm
@@ -121,9 +121,9 @@ rm LARGE/*.pgm
 ./psnr 0.5 0.9 0.6 200 200 >> realtime_experiment.log
 ./psnr 0.5 0.9 0.6 640 480 >> realtime_experiment.log
 ./psnr 0.5 0.9 0.6 1500 1350 >> realtime_experiment.log
-./psnr 0.5 0.9 0.6 200 200 -f ground_crew.h264 >> realtime_experiment.log
-./psnr 0.5 0.9 0.6 640 480 -f ground_crew.h264 >> realtime_experiment.log
-./psnr 0.5 0.9 0.6 1500 1350 -f ground_crew.h264 >> realtime_experiment.log
+./psnr 0.5 0.9 0.6 200 200 -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 0.5 0.9 0.6 640 480 -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 0.5 0.9 0.6 1500 1350 -f ground_crew.h264 >> ground_crew_experiment.log
 rm SMALL/*.pgm
 rm MEDIUM/*.pgm
 rm LARGE/*.pgm
@@ -140,9 +140,9 @@ rm LARGE/*.pgm
 ./psnr 0.5 0.9 0.4 200 200 >> realtime_experiment.log
 ./psnr 0.5 0.9 0.4 640 480 >> realtime_experiment.log
 ./psnr 0.5 0.9 0.4 1500 1350 >> realtime_experiment.log
-./psnr 0.5 0.9 0.4 200 200  -f ground_crew.h264 >> realtime_experiment.log
-./psnr 0.5 0.9 0.4 640 480  -f ground_crew.h264 >> realtime_experiment.log
-./psnr 0.5 0.9 0.4 1500 1350  -f ground_crew.h264 >> realtime_experiment.log
+./psnr 0.5 0.9 0.4 200 200  -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 0.5 0.9 0.4 640 480  -f ground_crew.h264 >> ground_crew_experiment.log
+./psnr 0.5 0.9 0.4 1500 1350  -f ground_crew.h264 >> ground_crew_experiment.log
 rm SMALL/*.pgm
 rm MEDIUM/*.pgm
 rm LARGE/*.pgm
